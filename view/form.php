1a8bbaca377a40a98c61d9cfb1d692a3e43cbbe9
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2023-09-13 16:26:14 +0100

    Updates registration form

--- view/form.php
+++ view/form.php
@@ -15,9 +15,10 @@
       <link rel="stylesheet" href="../assets/css/style.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
    </head>
-   <body>
+   <body style="overflow: scroll;">
       <div class="container">
-         <header>Register for a Course/Training</header>
+         <header>Registration Form</header>
+         <p style="color:red"><b>Altclick ICT World Course/Training Registration.</b></p>
          <div class="progress-bar">
             <div class="step">
                <p>
@@ -128,7 +129,7 @@
                         <p>*Virtual (The virtual training is a distance training that happens virtually)</p>
                         <p>*Private (The private training warrant us comming to your suitable place.)</p>
                      </div> -->
-                     <select name="mode" required id="mode">
+                     <select name="mode" required id="mode" onchange="courseAmount();">
                      <option selected disabled value="">Choose...</option>
                      <option value="Onsite">Onsite</option>
                      <option value="Virtual">Virtual</option>
@@ -169,172 +170,205 @@
      const duration = document.getElementById('duration');
    //   If training course if Frontend
      if(course.value=="Frontend Dev" && mode.value=="Onsite"){
-      amount.value="30";
-      duration.value="";
+      amount.value="150000";
+      duration.value="24 Weeks";
      }
      if(course.value=="Frontend Dev" && mode.value=="Virtual"){
-      amount.value="30";
+      amount.value="90000";
+      duration.value="24 Weeks";
      }
      if(course.value=="Frontend Dev" && mode.value=="Private"){
-      amount.value="30";
+      amount.value="230000";
+      duration.value="24 Weeks";
      }
    //   if training course is backend
      else if(course.value=="Backend Dev" && mode.value=="Onsite"){
-      amount.value="20";
-      duration.value="";
+      amount.value="200000";
+      duration.value="24 Weeks";
      }
      else if(course.value=="Backend Dev" && mode.value=="Virtual"){
-      amount.value="20";
-      duration.value="";
+      amount.value="180000";
+      duration.value="24 Weeks";
      }
      else if(course.value=="Backend Dev" && mode.value=="Private"){
-      amount.value="20";
-      duration.value="";
+      amount.value="300000";
+      duration.value="24 Weeks";
      }
      //   if training course is Mobile Apps Developent
-     else if(course.value=="Mobile Apps Deve" && mode.value=="Onsite"){
-      amount.value="10";
-      duration.value="";
+     else if(course.value=="Mobile Apps Dev" && mode.value=="Onsite"){
+      amount.value="250000";
+      duration.value="24 Weeks";
      }
-     else if(course.value=="Mobile Apps Deve" && mode.value=="Virtual"){
-      amount.value="10";
+     else if(course.value=="Mobile Apps Dev" && mode.value=="Virtual"){
+      amount.value="230000";
+      duration.value="24 Weeks";
      }
-     else if(course.value=="Mobile Apps Deve" && mode.value=="Private"){
-      amount.value="10"
+     else if(course.value=="Mobile Apps Dev" && mode.value=="Private"){
+      amount.value="350000"
+      duration.value="24 Weeks";
      }
    //   if training course is MERN
      else if(course.value=="MERN Stack Dev" && mode.value=="Onsite"){
-      amount.value="10";
-      duration.value="";
+      amount.value="250000";
+      duration.value="24 Weeks";
      }
      else if(course.value=="MERN Stack Dev" && mode.value=="Virtual"){
-      amount.value="10";
+      amount.value="230000";
+      duration.value="24 Weeks";
      }
      else if(course.value=="MERN Stack Dev" && mode.value=="Private"){
-      amount.value="10"
+      amount.value="350000"
+      duration.value="24 Weeks";
      }
    //   if training course is full-stall
      else if(course.value=="Full-stack Web Dev" && mode.value=="Onsite"){
-      amount.value="15";
-      duration.value="";
+      amount.value="250000";
+      duration.value="24 Weeks";
      }
      else if(course.value=="Full-stack Web Dev" && mode.value=="Virtual"){
-      amount.value="15";
+      amount.value="230000"
+      duration.value="24 Weeks";
      }
      else if(course.value=="Full-stack Web Dev" && mode.value=="Private"){
-      amount.value="15"
+      amount.value="350000"
      }
    //   if training course is PHP full-stack
      else if(course.value=="PHP Stack Dev" && mode.value=="Onsite"){
-      amount.value="40";
-      duration.value=""
+      amount.value="250000";
+      duration.value="24 Weeks";
      }
      else if(course.value=="PHP Stack Dev" && mode.value=="Virtual"){
-      amount.value="40";
+      amount.value="230000";
+      duration.value="24 Weeks";
      }
      else if(course.value=="PHP Stack Dev" && mode.value=="Private"){
-      amount.value="40"
+      amount.value="350000";
+      duration.value="24 Weeks";
      }
    //   if training course is data analysis
      else if(course.value=="Data Analysis" && mode.value=="Onsite"){
-      amount.value="50"
+      amount.value="150000";
+      duration.value="16 Weeks";
      }
      else if(course.value=="Data Analysis" && mode.value=="Virtual"){
-      amount.value="50"
+      amount.value="900000";
+      duration.value="16 Weeks";
      }
      else if(course.value=="Data Analysis" && mode.value=="Private"){
-      amount.value="50"
+      amount.value="230000";
+      duration.value="16 Weeks";
      }
    //   if training course is cyber security
      else if(course.value=="Cyber-Security" && mode.value=="Onsite"){
-      amount.value="60"
+      amount.value="300000";
+      duration.value="20 Weeks";
      }
      else if(course.value=="Cyber-Security" && mode.value=="Virtual"){
-      amount.value="60"
+      amount.value="230000";
+      duration.value="20 Weeks";
      }
      else if(course.value=="Cyber-Security" && mode.value=="Private"){
-      amount.value="60"
+      amount.value="350000";
+      duration.value="20 Weeks";
      }
    //   if training course is UI-UX
      else if(course.value=="Graphic Design UI-UX" && mode.value=="Onsite"){
-      amount.value="70"
+      amount.value="150000";
+      duration.value="16 Weeks";
      }
      else if(course.value=="Graphic Design UI-UX" && mode.value=="Virtual"){
-      amount.value="70"
+      amount.value="180000";
+      duration.value="16 Weeks";
      }
      else if(course.value=="Graphic Design UI-UX" && mode.value=="Private"){
-      amount.value="70"
+      amount.value="230000";
+      duration.value="16 Weeks";
      }
    //   if training course is business and entrepreneurship in IT
      else if(course.value=="Business and Entrepreneurship" && mode.value=="Onsite"){
-      amount.value="80"
+      amount.value="50000";
+      duration.value="16 Weeks";
      }
      else if(course.value=="Business and Entrepreneurship" && mode.value=="Virtual"){
-      amount.value="80"
+      return alert("This programme is currently not slided for virtual. It only takes place in our office location");
      }
      else if(course.value=="Business and Entrepreneurship" && mode.value=="Private"){
-      amount.value="80"
+      return alert("This programme is currently not slided for private. It only takes place in our office location");
      }
    //   if training course is certificate in computer
      else if(course.value=="Certificate in Computer" && mode.value=="Onsite"){
-      amount.value="50000"
+      amount.value="50000";
+      duration.value="8 Weeks";
      }
      else if(course.value=="Certificate in Computer" && mode.value=="Virtual"){
-      amount.value="50"
+      amount.value="45000";
+      duration.value="8 Weeks";
      }
      else if(course.value=="Certificate in Computer" && mode.value=="Private"){
-      amount.value="50"
+      amount.value="90000";
+      duration.value="8 Weeks";
      }
 
    //    if training course is diploma in data processing
      else if(course.value=="Diploma in Data Processing" && mode.value=="Onsite"){
-      amount.value="100"
+      amount.value="55000";
+      duration.value="12 Weeks";
      }
      else if(course.value=="Diploma in Data Processing" && mode.value=="Virtual"){
-      amount.value="100"
+      amount.value="50000";
+      duration.value="12 Weeks";
      }
      else if(course.value=="Diploma in Data Processing" && mode.value=="Private"){
-      amount.value="100"
+      amount.value="95000";
+      duration.value="12 Weeks";
      }
      //    if training course is diploma in sec studies
      else if(course.value=="Diploma in Sec Studies" && mode.value=="Onsite"){
-      amount.value="100"
+      amount.value="45000";
+      duration.value="12 Weeks";
      }
      else if(course.value=="Diploma in Sec Studies" && mode.value=="Virtual"){
-      amount.value="100"
+      amount.value="40000";
+      duration.value="12 Weeks";
      }
      else if(course.value=="Diploma in Sec Studies" && mode.value=="Private"){
-      amount.value="100"
+      amount.value="90000";
+      duration.value="12 Weeks";
      }
    //   if training course is video editing
      else if(course.value=="Video Editing" && mode.value=="Onsite"){
-      amount.value="55"
+      amount.value="60000";
+      duration.value="4 Weeks";
      }
      else if(course.value=="Video Editing" && mode.value=="Virtual"){
-      amount.value="55"
+      amount.value="50000";
+      duration.value="4 Weeks";
      }
      else if(course.value=="Video Editing" && mode.value=="Private"){
-      amount.value="55"
+      amount.value="100000";
+      duration.value="4 Weeks";
      }
    //   if training course is networking
      else if(course.value=="Networking" && mode.value=="Onsite"){
-      amount.value="65"
+      amount.value="75000";
+      duration.value="12 Weeks";
      }
      else if(course.value=="Networking" && mode.value=="Virtual"){
-      amount.value="65"
+      return alert("This programme is currently not slided for virtual. It only takes place onsite (in our office location)");
      }
      else if(course.value=="Networking" && mode.value=="Private"){
-      amount.value="65"
+      return alert("This programme is currently not slided for private. It only takes place onsite (in our office location)");
      }
    //   if training course is computer engineering
      else if(course.value=="Computer Engineering" && mode.value=="Onsite"){
-      amount.value="75"
+      amount.value="100000";
+      duration.value="16 Weeks";
      }
      else if(course.value=="Computer Engineering" && mode.value=="Virtual"){
-      amount.value="75"
+      return alert("This programme is currently not slided for virtual. It only takes place onsite (in our office location)")
      }
      else if(course.value=="Computer Engineering" && mode.value=="Private"){
-      amount.value="75"
+      return alert("This programme is currently not slided for private. It only takes place onsite (in our office location)")
      }
      else{
       amount.value="0.00"
